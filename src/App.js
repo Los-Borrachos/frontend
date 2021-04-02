9968a4df7a1665f20d1c6e277ddc58530bcd7f38
ghernandez24 <halex24a@bu.edu>
2021-04-02 13:15:51 -0400

    Working on bug for clientID route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './CSS/App.css';
 import { Route } from 'react-router-dom';
 import Nav from './Components/Nav.js';
-// import Clients from './Components/Clients.js';
+
 import Client from './Components/Client';
 import Clients from './Components/Clients';
 import Login from './Components/Login';
@@ -16,24 +16,20 @@ function App() {
 			</header>
 
 			<main className='app-body'>
-				{/* <Link to='/'>
-						{' '}
-						<img src={logo} alt='logo' />
-					</Link> */}
 				<div>
 					<Route path='/' exact>
 						<Login/>
 					</Route>
 					<Route path='/clients'>
 						<Clients />
-					</Route>
-					<Route path='/login'>
-						<Login />
-					</Route>
 					<Route
-						path='/client/:clientID'
+						path='/clients/:clientID' exact
 						render={(routerProps) => <Client match={routerProps.match} />}
 					/>
+					</Route>
+					{/* <Route path='/login'>
+						<Login /> */}
+					{/* </Route> */}
 					<Route path='/prospects'>
 						<Prospects />
 					</Route>
