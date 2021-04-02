0353045adc06be1839f5cd3b82c29a7523396cde
ghernandez24 <halex24a@bu.edu>
2021-04-02 17:27:02 -0400

    fixed rebase issue with route

--- src/App.js
+++ src/App.js
@@ -7,23 +7,21 @@ import Client from './Components/Client';
 import Clients from './Components/Clients';
 import Login from './Components/Login';
 import Prospect from './Components/Prospect'
-import Prospects from './Components/Prospects.js'
-import AddProspect from './Components/AddProspect.js'
+import Prospects from './Components/Prospects.js';
+import AddProspect from './Components/AddProspect.js';
 
 function App() {
 	const [token, setToken] = useState('');
-
-function logout(){
-	localStorage.removeItem("token")
-	setToken("")
-}
+	function logout() {
+		localStorage.removeItem('token');
+		setToken('');
+	}
 
 	return (
 		<div className='App'>
 			<header className='app-nav'>
-				<Nav logout = {logout} token = {token}/>
+				<Nav logout={logout} token={token} />
 			</header>
-
 			<main className='app-body'>
 				<Switch>
 					{!token ? (
@@ -32,15 +30,12 @@ function logout(){
 							path='/'
 							render={(props) => <Login {...props} setToken={setToken} />}
 						/>
-
 					) : (
 						<>
 							<Route path='/clients/:clientID' component={Client} />
-
-							<Route exact path={["/", "/clients"]} component={Clients} />
-
-							<Route path='/prospects' component={Prospects} />
-
+							<Route path='/prospects/:prospectID' component={Prospect} />
+							<Route exact path={['/', '/clients']} component={Clients} />
+							<Route exact path='/prospects' component={Prospects} />
 							<Route path='/add-prospect' component={AddProspect} />
 						</>
 					)}
@@ -48,19 +43,35 @@ function logout(){
 						path='*'
 						render={(props) => <Login {...props} setToken={setToken} />}
 					/>
-						<Route path='/clients/:clientID' component={Client}/>
-
-					<Route path='/prospects/:prospectID' component={Prospect}/>
-				
-					<Route path='/clients' component={Clients}/>
-				
-					<Route path='/prospects' component={Prospects}/>
-					
-					<Route path='/add-prospect' component={AddProspect}/>
 				</Switch>
 			</main>
 		</div>
 	);
+
+	// return (
+	// 	<div className='App'>
+	// 		<header className='app-nav'>
+	// 			<Nav />
+	// 		</header>
+
+	// 		<main className='app-body'>
+	// 			<Switch>
+	// 				<Route exact path='/' component={Login} />
+
+					// <Route
+					// 	path='/clients/:clientID' component={Client}/>
+
+					// <Route path='/prospects/:prospectID' component={Prospect}/>
+
+	// 				<Route path='/clients' component={Clients}/>
+
+	// 				<Route path='/prospects' component={Prospects}/>
+
+	// 				<Route path='/add-prospect' component={AddProspect}/>
+	// 			</Switch>
+	// 		</main>
+	// 	</div>
+	// );
 }
 
 export default App;
