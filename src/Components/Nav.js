e427415e76845c4a208b09d6cc74eb6e46f0881e
winschen98 <winston.chen98@gmail.com>
2021-04-02 00:21:24 -0700

    Revised template layout of app.css. Redesigned nav bar content. Added app logo.

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,23 +1,40 @@
 import React from 'react';
 import { Link, Switch } from 'react-router-dom';
 import '../CSS/Nav.css'
+import logo from '../images/Hound-logo.png'
 
 const Nav = () => {
 	return (
 		<div className='nav'>
+
 			<Link 
 			className='app-name'
 			to='/clients'
 			>
+				<img 
+				src={logo} 
+				className='logo'
+				alt='hound logo'
+				/>
 				<h1 >
 					Hound
 				</h1>
 			</Link>
-			<Link to='/clients'>Clients</Link>
-			<Link to='/prospects'>Prospects</Link>
+
+			<Link 
+			to='/clients'
+			className='clients-link'
+			>Clients</Link>
+
+			<Link 
+			to='/prospects'
+			className='prospects-link'
+			>Prospects</Link>
+
 			<Link to='/add-prospect'>
 				<button>Add prospect</button>
 			</Link>
+
 		</div>
 	);
 };
