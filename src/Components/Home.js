96c49b5bcb5bb900680fd02829bc3718b1fa07e5
ethanfincher <ethan.j.fincher@gmail.com>
2021-04-04 17:39:10 -0500

    spell check

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,6 @@ import '../CSS/Home.css'
 function Home() {
     return (
 			<div>
-                
 				<nav className='home-nav'>
 					<Link to='/signin'>
 						<button>Sign In</button>
@@ -18,7 +17,7 @@ function Home() {
 				</nav>
 				<main className='home-main'>
 					<div className='home-logo'>
-                        <h2>Welcome to Hound!</h2>
+						<h2>Welcome to Hound!</h2>
 						<Frame
 							animate={{ rotate: 360 }}
 							transition={{ duration: 3 }}
@@ -33,7 +32,14 @@ function Home() {
 					</div>
 					<div className='home-header'>
 						<p>
-							A Customer Relationship Management (or CRM) app is used to keep track of all of your companies relationships with current and future customers and clients. Our version, Hound, is a simple and easy and to use version of the classic technology that has been at the root of all customer based companies for decades. Just sign up, add a prospect and their information, move them over to the clients page, and see how easy it can be to keep track of all of your important buisness contacts!
+							A Customer Relationship Management (or CRM) app is used to keep
+							track of all of your company's relationships with current and
+							future customers and clients. Our version, Hound, is a simple and
+							easy to use version of the classic technology that has been at
+							the root of all customer based companies for decades. Just sign
+							up, add a prospect and their information, move them over to the
+							clients page, and see how easy it can be to keep track of all of
+							your important business contacts!
 						</p>
 					</div>
 				</main>
