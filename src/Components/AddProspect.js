0472e00ced09d1c13bf0a5f4a10e570e3f3f7113
winschen98 <winston.chen98@gmail.com>
2021-04-04 14:24:10 -0700

    Completed design and layout of add prospect page

--- src/Components/AddProspect.js
+++ src/Components/AddProspect.js
@@ -35,75 +35,106 @@ const AddProspect = () => {
 
 	return (
 		<div>
-			<h2>Add prospect:</h2>
-			<form onSubmit={handleSubmit} className='create-prospect'>
-				<label className='add-prospect-label'>Name: </label>
-				<input
-					onChange={handleChange}
-					name='name'
-					placeholder='Name'
-					value={prospect.name}
-					className='add-prospect-input'
-				/>
-				<label className='add-prospect-label'>Organization: </label>
-				<input
-					onChange={handleChange}
-					name='organization'
-					placeholder='Org'
-					className='add-prospect-input'
-				/>
-				<label className='add-prospect-label'>Email: </label>
-				<input
-					onChange={handleChange}
-					name='email'
-					placeholder='Email'
-					className='add-prospect-input'
-				/>
-				<label className='add-prospect-label'>Phone: </label>
-				<input
-					onChange={handleChange}
-					name='phoneNumber'
-					placeholder='Phone'
-					className='add-prospect-input'
-				/>
-				<label className='add-prospect-label'>Next Steps: </label>
-				<input
-					onChange={handleChange}
-					name='nextSteps'
-					placeholder='Step'
-					className='add-prospect-input'
-				/>
-				<label className='add-prospect-label'>Sales Stage: </label>
-				<input
-					onChange={handleChange}
-					name='salesStage'
-					placeholder='Stage'
-					className='add-prospect-input'
-				/>
-				<label className='add-prospect-label'>Total Revenue: </label>
-				<input
-					type='number'
-					onChange={handleChange}
-					name='totalRevenue'
-					placeholder='Revenue'
-					className='add-prospect-input'
-				/>
-				<label className='add-prospect-label'>Image Link: </label>
-				<input
-					
-					onChange={handleChange}
-					name='image'
-					placeholder='image link'
-					className='add-prospect-input'
-				/>
-				<input
-					id='submit'
-					type='submit'
-					value='Submit'
-					className='add-prospect-button'></input>
-				<Link to='/prospects'>
-					<button className='add-prospect-button'>Close</button>
-				</Link>
+			<h2>Add Prospect:</h2>
+			<form onSubmit={handleSubmit} className='prospect-form'>
+				<div className='add-prospect-col1'>
+
+					<div className='field'>
+						<label className='add-prospect-label'>Name: </label>
+						<input
+							onChange={handleChange}
+							name='name'
+							placeholder='Name'
+							value={prospect.name}
+							className='add-prospect-input'
+						/> 
+					</div>
+
+					<div className='field'>
+						<label className='add-prospect-label'>Organization: </label>
+						<input
+							onChange={handleChange}
+							name='organization'
+							placeholder='Org'
+							className='add-prospect-input'
+						/>
+					</div>
+
+					<div className='field'>
+					<label className='add-prospect-label'>Email: </label>
+					<input
+						onChange={handleChange}
+						name='email'
+						placeholder='Email'
+						className='add-prospect-input'
+					/>
+					</div>
+
+					<div className='field'>
+					<label className='add-prospect-label'>Phone: </label>
+					<input
+						onChange={handleChange}
+						name='phoneNumber'
+						placeholder='Phone'
+						className='add-prospect-input'
+					/>
+					</div>
+				</div>
+
+				<div className='add-prospect-col2'>
+					<div className='field'>
+					<label className='add-prospect-label'>Next Steps: </label>
+					<input
+						onChange={handleChange}
+						name='nextSteps'
+						placeholder='Step'
+						className='add-prospect-input'
+					/>
+					</div>
+
+					<div className='field'>
+					<label className='add-prospect-label'>Sales Stage: </label>
+					<input
+						onChange={handleChange}
+						name='salesStage'
+						placeholder='Stage'
+						className='add-prospect-input'
+					/>
+					</div>
+
+					<div className='field'>
+					<label className='add-prospect-label'>Total Revenue: </label>
+					<input
+						type='number'
+						onChange={handleChange}
+						name='totalRevenue'
+						placeholder='Revenue'
+						className='add-prospect-input'
+					/>
+					</div>
+
+					<div className='field'>
+					<label className='add-prospect-label'>Image Link: </label>
+					<input
+						onChange={handleChange}
+						name='image'
+						placeholder='image link'
+						className='add-prospect-input'
+					/>
+					</div>
+				</div>
+
+				<div className='form-buttons'>
+					<button
+						id='submit'
+						type='submit'
+						value='Submit'
+						className='add-prospect-button'>Submit
+					</button>
+					<Link to='/prospects'>
+						<button className='add-prospect-button'>Close</button>
+					</Link>
+				</div>
 			</form>
 		</div>
 	);
